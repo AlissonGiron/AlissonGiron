86bbe30e8f23c21a58943136e83926382cc201d2
Alisson Giron <alissongiron.hs@gmail.com>
2018-07-06 20:21:34 -0300

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlissonGiron.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
